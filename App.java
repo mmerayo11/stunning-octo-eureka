a366705ec8f418eb33f239b5b4933c9513746eb5
Mario <mmerayo1803@gmail.com>
2023-01-26 12:32:35 +0100

    cambios readme y app

--- App.java
+++ App.java
@@ -1,9 +1,13 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Mario Merayo Barredo");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("26/01/2023");
+    System.out.println("Ponferrada");
+  
   }
   
 }
